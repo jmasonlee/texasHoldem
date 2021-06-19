e269016f12f2f0ae70894ca5c876bdaedf23f332
jmasonlee <jmasonlee@gmail.com>
2021-06-19 09:55:00 -0600

    Parse game with multiple players

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,20 +1,52 @@
-import { PokerHand } from "./PokerHand";
+import {PokerHand} from "./PokerHand";
 
 export default class Parser {
     static parse(input: string) {
         const inputLines: string[] = input.trim().split("\n")
-        const playerLines: string[] = inputLines.slice(1)
-        let pokerHands = this.buildHands(playerLines);
+
+        let pokerHands = this.buildHands(inputLines);
 
         return pokerHands;
     }
 
-    private static buildHands(playerLines: string[]) {
-        let pokerHands = [];
-        for (const line in playerLines) {
-            let pokerHand = new PokerHand("Jayne", ["QH", "KS", "3H", "2C", "AD", "TC", "4D"]);
+    private static buildHands(inputLines: string[]) {
+
+        let parsedElements: string[][] = []
+
+        inputLines.forEach(line => {
+            let elements: string[] = this.splitLinesIntoElements(line)
+            parsedElements.push(elements)
+        })
+
+        let communityCards: string[] = parsedElements[0]
+        let playersAndPocketCards: string[][] = parsedElements.slice(1);
+        return this.createHands(playersAndPocketCards, communityCards);
+    }
+
+    private static createHands(playersAndPocketCards: string[][], communityCards: string[]) :PokerHand[]{
+        let pokerHands: PokerHand[] = [];
+        playersAndPocketCards.forEach(line => {
+            let playerName: string = line[0]
+            let playerCards = this.getPotentialPlayerCards(communityCards, line);
+
+            let pokerHand = new PokerHand(playerName, playerCards);
             pokerHands.push(pokerHand)
-        }
-        return pokerHands;
+        })
+        return pokerHands
+    }
+
+    private static getPotentialPlayerCards(communityCards: string[], line: string[]) {
+        let cards: string[] = []
+        communityCards.forEach(card => cards.push(card))
+        line.slice(1).forEach(card => cards.push(card))
+        return cards;
+    }
+
+    private static splitLinesIntoElements(line: string) {
+        return line.trim().split(/(\s+)/).filter(this.containsWhitespace);
+    }
+
+    private static containsWhitespace(token) {
+        return token.match(/^ *$/) == null;
     }
 }
--- src/__tests__/parser.test.ts
+++ src/__tests__/parser.test.ts
@@ -1,19 +1,25 @@
 import Parser from '../Parser'
 import {PokerHand} from "../PokerHand";
 
-//Multiple Players
 //Nonstandard whitespace
 //Nonstandard formatting
 describe('The input parser', function () {
-    it('returns a poker hand when given input with a single player', function () {
+    it('returns a poker hand for each player given', function () {
         let input: string = `
         QH KS 3H 2C AD
         Jayne TC 4D
+        Zoe   3S 5C
+        Mal   KD KH
+        Wash  JD TH
         `;
 
-        let pokerHand: PokerHand = new PokerHand("Jayne", [ "QH", "KS", "3H", "2C", "AD", "TC", "4D"]);
+        let jaynesHand: PokerHand = new PokerHand("Jayne", [ "QH", "KS", "3H", "2C", "AD", "TC", "4D"]);
+        let zoesHand: PokerHand = new PokerHand("Zoe", [ "QH", "KS", "3H", "2C", "AD", "3S", "5C"]);
+        let malsHand: PokerHand = new PokerHand("Mal", [ "QH", "KS", "3H", "2C", "AD", "KD", "KH"]);
+        let washsHand: PokerHand = new PokerHand("Wash", [ "QH", "KS", "3H", "2C", "AD", "JD", "TH"]);
 
-        expect(Parser.parse(input)).toEqual([pokerHand])
+        let pokerHands = [jaynesHand, zoesHand, malsHand, washsHand];
+        expect(Parser.parse(input)).toEqual(pokerHands)
     });
 
     it('returns an empty list when given input with no players', function () {
