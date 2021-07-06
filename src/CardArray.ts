8d050d779406ed96f7e231fd64c9ffcbca91f4d8
jmasonlee <jmasonlee@gmail.com>
2021-07-06 15:20:57 -0600

    Add Ability to count repeated values in CardArray

--- src/CardArray.ts
+++ src/CardArray.ts
@@ -15,9 +15,31 @@ export class CardArray {
         let cards:Card[] = CardArray.getCardsFromSymbols([...symbols])
         return new CardArray(cards)
     }
+    public countRepeatedValues() {
+        let cardValues: number[] = this._cards.map(c => c.value)
+        return CardArray.countDuplicatesInArray(cardValues) as Map<number, number>
+    }
 
     private static getCardsFromSymbols(symbols: string[]):Card[] {
         const cards = symbols.map(c => getCardMatchingSymbol(c))
         return cards;
+    }
+    private static countDuplicatesInArray(entries: any[]): Map<any, number> {
+        let entryCount: Map<any, number> = new Map<any, number>()
+        entries.forEach(property => {
+            let currentCount = entryCount.get(property)
+            currentCount = currentCount ? currentCount + 1 : 1
+            entryCount.set(property, currentCount)
+        })
+        return entryCount;
+    }
+
+    public filterDuplicateValues() {
+        let cardsWithuniqueValues: Card[] = []
+        const uniqueValues: number[] = [...new Set(this._cards.map(c => c.value))]
+        uniqueValues.forEach(value => {
+            cardsWithuniqueValues.push(this._cards.find(card => card.value === value))
+        })
+        return new CardArray(cardsWithuniqueValues);
     }
 }
--- src/__tests__/CardArray.test.ts
+++ src/__tests__/CardArray.test.ts
@@ -12,4 +12,15 @@ describe('The CardArray', () => {
 
         expect(expectedCards).toEqual(actualCards)
     })
+    it('Counts the number of times a Card of a certain value repeats in the array', () => {
+        const expectedValueCounts: Map<number, number> = new Map<number, number>()
+        expectedValueCounts.set(2, 3)
+        expectedValueCounts.set(5, 2)
+        expectedValueCounts.set(13, 2)
+        const cards = CardArray.getCardArrayFromSymbols(['2D', '2S', '2C', '5H', '5C', 'KS', 'KH'])
+
+        const actualValueCounts: Map<number, number> = cards.countRepeatedValues()
+
+        expect(actualValueCounts).toEqual(expectedValueCounts)
+    })
 })
