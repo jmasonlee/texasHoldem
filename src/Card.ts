06d04f5c0182cae2835bedceb913cba5bdbf0a78
jmasonlee <jmasonlee@gmail.com>
2021-07-06 15:20:57 -0600

    Create object to represent Card

---
+++ src/Card.ts
@@ -0,0 +1,45 @@
+export interface Card {
+    value: (number | undefined)
+    suit: (string | undefined)
+    valueName: (string | undefined)
+}
+export function compareCards(c1: Card, c2: Card) {
+    if (c1.value < c2.value) {
+        return 1
+    }
+    if (c2.value < c1.value) {
+        return -1
+    } else {
+        return 0
+    }
+}
+
+export function getCardMatchingSymbol(cardSymbol: string): Card {
+    const card:Card = {value: undefined, valueName: undefined, suit: cardSymbol[1]}
+    switch (cardSymbol[0]) {
+        case 'T':
+            card.value = 10
+            card.valueName = '10'
+            break
+        case 'J':
+            card.value = 11
+            card.valueName = 'Jack'
+            break
+        case 'Q':
+            card.value = 12
+            card.valueName = 'Queen'
+            break
+        case 'K':
+            card.value = 13
+            card.valueName = 'King'
+            break
+        case 'A':
+            card.value = 14
+            card.valueName = 'Ace'
+            break
+        default:
+            card.value = +cardSymbol[0]
+            card.valueName = cardSymbol[0]
+    }
+    return card
+}
---
+++ src/__tests__/Card.test.ts
@@ -0,0 +1,24 @@
+import {Card, compareCards, getCardMatchingSymbol} from "../Card"
+
+describe("A Card", () => {
+    it('assigns the correct relative value on creation', () => {
+        const unorderedCardSymbols: string[] = ['KD', '5D', '2D', 'AD', 'QD', '6D', '7D', '4D', '3D', '8D', 'JD', 'TD', '9D']
+        const expectedOutputOrder: Card[] =
+            ['2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'JD', 'QD', 'KD', 'AD'].map(c => getCardMatchingSymbol(c))
+
+        const actualCardOrder = unorderedCardSymbols
+            .map(c => getCardMatchingSymbol(c)).sort(compareCards).reverse()
+
+        expect(actualCardOrder).toEqual(expectedOutputOrder)
+    })
+
+    it('assigns the correct value name for royal cards', () => {
+        const cardSymbols: string[] = ['KD', 'AD', 'QD', 'JD', 'TD']
+        const cards: Card[] = cardSymbols.map(c => getCardMatchingSymbol(c))
+        const expectedValueNames = ['King', 'Ace', 'Queen', 'Jack', '10']
+        const actualValueNames:string[] = cards.map(c => c.valueName)
+
+        expect(actualValueNames).toEqual(expectedValueNames)
+    })
+
+})
