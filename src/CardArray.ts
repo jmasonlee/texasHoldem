9c49925e681a567047dbb5a4e0dff6f8ca9d4049
jmasonlee <jmasonlee@gmail.com>
2021-07-06 15:20:57 -0600

    Add ability for CardArray to detect sequences

--- src/CardArray.ts
+++ src/CardArray.ts
@@ -15,6 +15,28 @@ export class CardArray {
         let cards:Card[] = CardArray.getCardsFromSymbols([...symbols])
         return new CardArray(cards)
     }
+
+    getCardsInSequence(): CardArray {
+        let longestSequence: Card[] = []
+        let sequence: Card[] = []
+
+        const sortedUniqueCards = this.filterDuplicateValues().cards.sort(compareCards)
+
+        sortedUniqueCards.forEach((card: Card, index: number) => {
+            const nextCard = sequence[sequence.length - 1];
+            if (index === 0 || card.value === (nextCard.value) - 1) {
+                sequence.push(card)
+            } else if(longestSequence.length < sequence.length){
+                longestSequence = sequence
+                sequence = [card]
+            } else{
+                sequence = [card]
+            }
+        })
+
+        const cardsInSequence = sequence.length < longestSequence.length? longestSequence : sequence
+        return new CardArray(cardsInSequence)
+    }
     public countRepeatedValues() {
         let cardValues: number[] = this._cards.map(c => c.value)
         return CardArray.countDuplicatesInArray(cardValues) as Map<number, number>
--- src/__tests__/CardArray.test.ts
+++ src/__tests__/CardArray.test.ts
@@ -12,6 +12,22 @@ describe('The CardArray', () => {
 
         expect(expectedCards).toEqual(actualCards)
     })
+
+    it('pulls the longest sequence of Cards from its Card[]', () => {
+        const cards: CardArray = CardArray.getCardArrayFromSymbols(['KH', '6H', '5H', '4H', '3H', '2C'])
+        const expectedSequence: Card[] = [
+            getCardMatchingSymbol('6H'),
+            getCardMatchingSymbol('5H'),
+            getCardMatchingSymbol('4H'),
+            getCardMatchingSymbol('3H'),
+            getCardMatchingSymbol('2C'),
+        ]
+
+        const actualSequence = cards.getCardsInSequence().cards
+
+        expect(expectedSequence).toEqual(actualSequence)
+    })
+
     it('Counts the number of times a Card of a certain value repeats in the array', () => {
         const expectedValueCounts: Map<number, number> = new Map<number, number>()
         expectedValueCounts.set(2, 3)
@@ -23,4 +39,12 @@ describe('The CardArray', () => {
 
         expect(actualValueCounts).toEqual(expectedValueCounts)
     })
+    it('Filters by duplicate values', () => {
+        const cards = CardArray.getCardArrayFromSymbols(['2D', '2S', '2C', '5H', '5C', 'KS', 'KH'])
+        const expectedFilteredArray: CardArray = CardArray.getCardArrayFromSymbols(['2D', '5H', 'KS'])
+        const actualFilteredArray: CardArray = cards.filterDuplicateValues()
+
+        expect(expectedFilteredArray).toEqual(actualFilteredArray)
+    })
+
 })
