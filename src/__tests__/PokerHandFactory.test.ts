022569d15b56ea090cf90c72aef324b2a6b62fa2
jmasonlee <jmasonlee@gmail.com>
2021-07-06 18:47:23 -0600

    Add ability to detect kickers

--- src/__tests__/PokerHandFactory.test.ts
+++ src/__tests__/PokerHandFactory.test.ts
@@ -1,79 +1,93 @@
 import {PokerHand} from "../pokerHands/PokerHand";
+import {Card} from "../Card";
 import {PokerHandFactory} from "../pokerHands/PokerHandFactory";
+import {CardArray} from "../CardArray";
 
-function checkHand(cardSymbols: string[], expectedHandDescription: string) {
+function checkHand(cardSymbols: string[], expectedHandDescription: string, expectedKickers: Card[]) {
     const pokerHand: PokerHand = PokerHandFactory.createPokerHand('River', cardSymbols)
+
     expect(pokerHand.describeHand()).toEqual(expectedHandDescription)
+    expect(pokerHand.kickers).toEqual(expectedKickers)
 }
 
 describe("PokerHand", () => {
     it('Will use the cards value name', () => {
         const expectedHandDescription = 'River High Card Queen';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols(['9D', '8C', '6D', '5D']).cards
         const cardSymbols = ['8C', '2S', '3H', '6D', '5D', '9D', 'QD'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly name a pair of a lower value', () => {
         const expectedHandDescription = 'River Pair 2';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols(['9D', '8C', '7S']).cards
         const cardSymbols = ['8C', '7S', '3H', '2D', '5D', '9D', '2C'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly name Three of a kind', () => {
         const expectedHandDescription = 'River Three of a Kind 9';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols(['8C', '7S']).cards
         const cardSymbols = ['8C', '7S', '3H', '2D', '9S', '9D', '9C'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly name Four of a Kind', () => {
         const expectedHandDescription = 'River Four of a Kind 9';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols(['8D']).cards
         const cardSymbols = ['8D', '7D', '3D', '9H', '9S', '9D', '9C'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly name a Full House', () => {
         const expectedHandDescription = 'River Full House 8 9';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols([]).cards
         const cardSymbols = ['8D', '8H', '8C', '9H', '9S', 'AD', 'TC'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly name a Straight', () => {
         const expectedHandDescription = 'River Straight Ace';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols([]).cards
         const cardSymbols = ['KD', 'QH', 'JC', 'TH', '9S', 'AD', 'TC'];
 
-        checkHand(cardSymbols, expectedHandDescription)
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly name a Straight where ace is low', () => {
         const expectedHandDescription = 'River Straight 5';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols([]).cards
         const cardSymbols = ['KD', 'QH', '5C', '4H', '3S', 'AD', '2C'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly create a Flush', () => {
         const expectedHandDescription = 'River Flush King';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols([]).cards
         const cardSymbols = ['KH', 'QH', '5H', '4H', '3H', 'AD', '2C'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly create a Straight Flush', () => {
         const expectedHandDescription = 'River Straight Flush King';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols([]).cards
         const cardSymbols = ['KH', 'QH', 'JH', 'TH', '9H', 'AD', '2C'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 
     it('Will correctly create a Royal Flush', () => {
         const expectedHandDescription = 'River Royal Flush';
+        const expectedKickers: Card[] = CardArray.getCardArrayFromSymbols([]).cards
         const cardSymbols = ['KH', 'QH', 'JH', 'TH', '9H', 'AH', '2H'];
 
-        checkHand(cardSymbols, expectedHandDescription);
+        checkHand(cardSymbols, expectedHandDescription, expectedKickers);
     })
 })
--- src/pokerHands/PokerHand.ts
+++ src/pokerHands/PokerHand.ts
@@ -19,9 +19,12 @@ export abstract class PokerHand {
     rankingCards: CardArray
     handType: HandType
     handName: string
+    kickers: Card[] = [];
+
      protected constructor(playerName: string, cards: CardArray) {
         this.playerName = playerName
         this.rankingCards = this.getRankingCards(cards)
+        this.kickers = this.getKickers(cards.cards, this.rankingCards)
     }
 
     protected abstract getRankingCards(cards: CardArray): CardArray
@@ -30,9 +33,27 @@ export abstract class PokerHand {
         return `${this.playerName} ${this.handName}${(this.getStringRepresentationOfRankingCards())}`
     }
 
+    getKickerScore() {
+        return this.kickers.map((kicker: Card) => kicker.value).reduce((a: number, b: number) => a + b, 0);
+    }
 
     protected getStringRepresentationOfRankingCards(): string {
         return ` ${this.rankingCards.cards[0].valueName}`
+    }
+
+    private getKickers(cards: Card[], rankingCards: CardArray): Card[] {
+        const kickers = [...cards];
+        this.removeRankingCards(kickers, rankingCards.cards);
+        const numKickers = PokerHand.HAND_SIZE - rankingCards.cards.length;
+        return PokerHand.getKickersWithHighestValue(kickers, numKickers);
+    }
+
+    private static getKickersWithHighestValue(cards: Card[], numKickers: number): Card[] {
+        return cards.sort(compareCards).slice(0, numKickers);
+    }
+
+    private removeRankingCards(cards: Card[], rankingCards: Card[]) {
+        rankingCards.map(c => cards.splice(cards.indexOf(c), 1))
     }
 }
 
